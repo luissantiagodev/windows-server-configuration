347b12def3277a79f754c9062a45cc6c1845298b
luissantiagodev <luissantiagodev@gmail.com>
2021-03-23 18:28:50 -0600

    testing

--- helpers/FirebaseHelper.js
+++ helpers/FirebaseHelper.js
@@ -4,6 +4,7 @@ const serviceAccount = require(`${__dirname}/../config/firebase.json`);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://mapfre-a072b-default-rtdb.firebaseio.com/",
     storageBucket: "gs://mapfre-a072b.appspot.com",
 });
 
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const firebaseHelper = require('./helpers/FirebaseHelper')
 const fs = require('fs');
 const request = require('request');
 const cmd = require('node-cmd');
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 const EXCEL_EXE = " \"C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE\""
 
 require('core-js/modules/es.promise');
@@ -16,12 +20,17 @@ const Excel = require('exceljs/dist/es5');
 const uuid = require("uuid-v4");
 
 
+app.listen(PORT, () => console.log(`Listing in port ${PORT}`));
+
 firebaseHelper.admin.database().ref("listener_documents").on('value', (snapshot) => {
-    console.log(snapshot.val())
-    cmd.run(EXCEL_EXE, (err, data, stderr) => {
-            console.log('Running excel')
-        }
-    );
+
+    if (snapshot.exists()) {
+        console.log(snapshot.val())
+        cmd.run(EXCEL_EXE, (err, data, stderr) => {
+                console.log('Running excel')
+            }
+        );
+    }
 
 })
 
