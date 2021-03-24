fd37e7811b0e289a3fdab011bbcba31aac6fcd64
luissantiagodev <luissantiagodev@gmail.com>
2021-03-23 19:10:06 -0600

    testing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cmd = require('node-cmd');
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+const http = require('http');
 const EXCEL_EXE = " \"C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE\""
 
 require('core-js/modules/es.promise');
@@ -25,21 +25,20 @@ app.listen(PORT, () => console.log(`Listing in port ${PORT}`));
 firebaseHelper.admin.database().ref("listener_documents").child("document").on('value', (snapshot) => {
     if (snapshot.exists()) {
 
-
         let data = snapshot.val()
 
         let path = `${__dirname}/src/${data.id}.xls`
-
-
         let command = `${EXCEL_EXE} "${path}"`
+
         console.log(path)
         console.log(command)
 
         download(data.url, path, () => {
-            cmd.run(command, (err, data, stderr) => {
-                    firebaseHelper.admin.database().ref("listener_documents").child("document").remove()
-                }
-            );
+
+            /* cmd.run(command, (err, data, stderr) => {
+                     firebaseHelper.admin.database().ref("listener_documents").child("document").remove()
+                 }
+             );*/
         })
 
 
@@ -49,7 +48,8 @@ firebaseHelper.admin.database().ref("listener_documents").child("document").on('
 
 
 const download = (uri, filename, callback) => {
-    request.head(uri, (err, res, body) => {
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    const file = fs.createWriteStream(filename);
+    http.get(uri, (response) => {
+        response.pipe(file);
     });
 };
