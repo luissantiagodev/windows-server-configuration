c245cf433aee37984393dd88e6b821fee8eb3779
luissantiagodev <luissantiagodev@gmail.com>
2021-03-23 18:05:41 -0600

    testing

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const firebaseHelper = require('./helpers/FirebaseHelper')
+const fs = require('fs');
+const request = require('request');
+const cmd = require('node-cmd');
+const EXCEL_EXE = " \"C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE\""
+
+require('core-js/modules/es.promise');
+require('core-js/modules/es.string.includes');
+require('core-js/modules/es.object.assign');
+require('core-js/modules/es.object.keys');
+require('core-js/modules/es.symbol');
+require('core-js/modules/es.symbol.async-iterator');
+require('regenerator-runtime/runtime');
+
+const Excel = require('exceljs/dist/es5');
+const uuid = require("uuid-v4");
 
 
 firebaseHelper.admin.database().ref("listener_documents").on('value', (snapshot) => {
     console.log(snapshot.val())
+    cmd.run(EXCEL_EXE, (err, data, stderr) => {
+            console.log('Running excel')
+        }
+    );
+
 })
+
+
+const download = (uri, filename, callback) => {
+    request.head(uri, (err, res, body) => {
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+};
